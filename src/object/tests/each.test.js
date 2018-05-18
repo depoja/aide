70b84cbf585b699411fd44d4dbf31210c2faee60
Klint Mane <klintmane@gmail.com>
2018-05-18 11:00:58 +0200

    Add object tests

--- src/object/tests/each.test.js
+++ src/object/tests/each.test.js
@@ -1,5 +1,19 @@
-import { each } from "../each";
+import { each, eachKey, eachValue } from "../each";
 
-describe("each", () => {
-  it("works", () => {});
+describe("eachKey", () => {
+  it("works", () => {
+    let keys = "";
+    eachKey({ a: 1, b: 2 }, key => (keys += `_${key}`));
+
+    expect(keys).toBe("_a_b");
+  });
+});
+
+describe("eachValue", () => {
+  it("works", () => {
+    let sum;
+    eachKey({ a: 1, b: 2 }, val => (sum += val));
+
+    expect(sum).toBe(3);
+  });
 });
