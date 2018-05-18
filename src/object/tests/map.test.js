b80bf93def91924a1ca8a23d36c1794e81c67e04
Klint Mane <klintmane@gmail.com>
2018-05-18 10:54:32 +0200

    Add map tests

--- src/object/tests/map.test.js
+++ src/object/tests/map.test.js
@@ -1,9 +1,31 @@
-import { map } from "../map";
+import { map, mapValues, mapKeys } from "../map";
 
 describe("map", () => {
   it("works", () => {
     const obj = { a: 1, b: 2 };
     const mapped = map(obj, key => key.toUpperCase(), val => val * 100);
+
+    expect(mapped).not.toBe(obj);
     expect(mapped).toEqual({ A: 100, B: 200 });
+  });
+});
+
+describe("mapKeys", () => {
+  it("works", () => {
+    const obj = { a: 1, b: 2 };
+    const mapped = mapKeys(obj, key => key.toUpperCase());
+
+    expect(mapped).not.toBe(obj);
+    expect(mapped).toEqual({ A: 1, B: 2 });
+  });
+});
+
+describe("mapValues", () => {
+  it("works", () => {
+    const obj = { a: 1, b: 2 };
+    const mapped = mapValues(obj, val => val * 100);
+
+    expect(mapped).not.toBe(obj);
+    expect(mapped).toEqual({ a: 100, b: 200 });
   });
 });
