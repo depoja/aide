1bb8726ce64c3a378af3e2b83a02e9cd72d656b3
Klint Mane <klintmane@gmail.com>
2018-05-21 12:16:14 +0200

    v0.0.3

--- src/object/get.js
+++ src/object/get.js
@@ -2,4 +2,4 @@ import { reduce } from "../array/reduce";
 import { parsePath } from "./path";
 
 export const get = (obj, path) =>
-  reduce(parsePath(path), (val, key) => val && val[key], obj);
+  reduce(parsePath(path), (val, key) => (val ? val[key] : undefined), obj);
